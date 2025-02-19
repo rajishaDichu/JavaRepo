0b3d452e1f33be4f8857e9a181a6bd93b1086246
rajis <rajis@192.168.1.2>
2025-02-19 11:17:05 +0530

    Arraylist

---
+++ JavaProjectMain/src/main/java/collection/ArrayListExmple.java
@@ -0,0 +1,21 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class ArrayListExmple {
+
+	public static void main(String[] args) 
+	{
+		ArrayList<String>obj1=new ArrayList<String>();
+		obj1.add("Apple");
+		obj1.add("Orange");
+		obj1.add("Grapes");
+		obj1.add("Kiwi");
+		obj1.add("Banana");
+		
+		
+		
+		
+	}
+
+}
--- JavaProjectMain/src/main/java/collection/GenericCollectionEx.java
+++ JavaProjectMain/src/main/java/collection/GenericCollectionEx.java
@@ -10,7 +10,22 @@ public static void main(String[] args)
 	List <String>obj=new ArrayList<String>();
 	obj.add("Rajisha");
 	obj.add("Dichu");
+	obj.add("Anu");
+	obj.add("Manu");
+	obj.add("Vichu");
+	obj.add("Rajisha");
 	System.out.println(obj);
+	obj.set(2, "Meenu");//replace the value from index 2 with the new value
+	System.out.println(obj);
+	System.out.println(obj.indexOf("Rajisha"));
+	System.out.println(obj.lastIndexOf("Rajisha"));
+	obj.remove(0);//to remove the value from the mentioned index
+    System.out.println(obj);
+	obj.remove("Vichu");
+	System.out.println(obj);
+	System.out.println(obj.get(2));//to get the object in the mentioned index
+	System.out.println(obj.contains("tree"));//to check whether the list contains the given object
+
 }
 
 }
